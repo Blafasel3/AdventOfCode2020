1a7e02bf20194aea07d2274eab7edfd7078862e2
Maximilian <extern.lenhardt_maximilian@allianz.de>
2020-12-25 14:05:04 +0100

    master: Added solution for day 25. Only one part :)

--- src/test/kotlin/advent/of/code/day25/Day25Test.kt
+++ src/test/kotlin/advent/of/code/day25/Day25Test.kt
@@ -1,4 +1,36 @@
 package advent.of.code.day25
 
+import org.junit.Test
+
 class Day25Test {
+    private val remainder = 20201227L
+
+    @Test
+    fun `part 1`() {
+        val cardPublicKey = 11404017L
+        val cardLoopSize = findLoopSize(cardPublicKey)
+        val doorPublicKey = 13768789L
+        val doorLoopSize = findLoopSize(doorPublicKey)
+
+        println(transformPublicKeyToEncryptionKey(cardLoopSize, doorPublicKey))
+        println(transformPublicKeyToEncryptionKey(doorLoopSize, cardPublicKey))
+    }
+
+    private fun findLoopSize(publicKey: Long, subjectNumber: Long = 7L): Int {
+        var value = 1L
+        var loopSize = 0
+        while (value != publicKey) {
+            loopSize++
+            value = (value.times(subjectNumber)).rem(remainder)
+        }
+        return loopSize
+    }
+
+    private fun transformPublicKeyToEncryptionKey(otherDeviceLoopSize: Int, subjectNumber: Long): Long {
+        var value = 1L
+        for (i in 0 until otherDeviceLoopSize) {
+            value = (value.times(subjectNumber)).rem(remainder)
+        }
+        return value
+    }
 }
